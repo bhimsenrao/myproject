161ed9fb792dc80b33777fad539a1b2899ef2ac1
bhimsen <brkaran2k2@gmail.com>
2024-09-13 11:03:15 +0530

    two files are commited

--- firstCode.py
+++ firstCode.py
@@ -5,3 +5,4 @@ y=20
 print("First value %d second value %d"%(x,y))
 z=x+y
 print("Sum of numbers %d"%(z))
+print("Square of %d =%d"%(z,(z*z)))
---
+++ secondCode.py
@@ -0,0 +1,9 @@
+# second program 
+print("This code will show ID, type of data")
+
+x=10
+print(id(x))
+print(type(x))
+print("Value ",x)
+x=x+1
+print("Incremented ",x)
